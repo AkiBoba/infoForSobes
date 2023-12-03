53f7d4da4dabc73e3bef9e0623f973432044af4a
Vladimir Likhachev <lettervl@ya.ru>
2023-12-03 12:56:38 +0300

    Qeue

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -7,3 +7,9 @@ M
 src/main/java/src/Model1.java,9\d\9d4ca7dc05e12f55e358101923b9964ba5c6dccc
 L
 src/main/java/src/Model.java,4\a\4a6db681a03eb08e74295accab362440b898270d
+H
+src/main/java/intern/OOP,7\0\70bcf9c82346a5037da5c2a3091999806f436cd2
+M
+src/main/java/Calculator.java,9\0\90877c721c4ed22cafdaaa54f21e2941f52e72d5
+]
+-src/main/java/src/fabricmetod/FabricImpl.java,a\4\a49895607a9935f938870241a514e6a9eb02a3f7
---
+++ src/test/java/src/fabricmetod/FabricImplTest.java
@@ -0,0 +1,16 @@
+package src.fabricmetod;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FabricImplTest {
+
+    @Test
+    void getVehicle() {
+        Fabric fabric = new FabricImpl();
+        Vehicle vehicle = new Moto();
+        assertThat(vehicle.create()).isEqualTo(fabric.getVehicle(2).create());
+    }
+}
