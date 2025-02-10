f26496854cef61f4ffcc3b641aebedf80c2e6d6c
AkiBoba <lettervl@ya.ru>
2025-02-10 14:10:38 +0300

    Кубер

--- src/main/java/src/threads/ImMain.java
+++ src/main/java/src/threads/ImMain.java
@@ -2,8 +2,9 @@ package src.threads;
 
 public class ImMain {
     public static void main(String[] args) {
-        ImmutableClass immutableClass10 = new ImmutableClass(10);
-        int n = immutableClass10.getNum();
-        System.out.println(n);
+//        ImmutableClass immutableClass10 = new ImmutableClass(10);
+//        int n = immutableClass10.getNum();
+        String[] strings = new String[10];
+        System.out.println(strings.length);
     }
 }
